7eb3ce8f0a975cec1226237df2f7952fbef04fea
Amir Alami <abstract.stream@gmail.com>
2020-01-22 01:35:42 +0330

    Copy webpack plugin added

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
-  }
+  },
+  plugins: [new CopyWebpackPlugin([{ from: "public" }])]
 };
